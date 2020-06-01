4ede5911e0d1552edea3d33c094478f23593e44f
Michael Suárez <xtreme.maicolxt94@gmail.com>
2020-06-01 03:25:35 -0500

    changes

---
+++ deleted.ts
@@ -0,0 +1,3 @@
+export var data = [
+	"user0003","user0004"
+]
---
+++ users.ts
@@ -0,0 +1,49 @@
+export var data = {
+
+	user0001: {
+		finish: 1591340400000
+	},
+
+	user0002: {
+		finish: 1591426800000
+	},
+
+	user0005: {
+		finish: 1591215000000
+	},
+
+	user0006: {
+		finish:1591421400000
+	},
+
+	user0007: {
+		finish: 1591483800000,
+		throttle: 6
+	},
+
+	user0008: {
+		finish: 1591483800000,
+		throttle: 6
+	},
+
+	user0009: {
+		finish: 1591565400000
+	},
+
+	user0010: {
+		finish: 1591486200000
+	},
+
+	user0012: {
+		finish: 1591498800000
+	},
+
+	user0013: {
+		finish: 1591560000000
+	},
+
+	user0014: {
+		finish: 1591567200000,
+		throttle: 7
+	}
+}
