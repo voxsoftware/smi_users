f9a9aba70669ade94caf46f566825cf682f87fba
Michael James <xtreme.maicolxt94@gmail.com>
2020-06-07 01:15:13 -0500

    update users

--- users.ts
+++ users.ts
@@ -1,53 +1,95 @@
 export var data = {
 
 	user0001: {
-		expires: 1591340400000
+		expires: "2020-05-12"
 	},
 
 	user0002: {
-		expires: 1591426800000
+		expires: "2020-06-04"
 	},
 
 	user0005: {
-		expires: 1591215000000
+		expires: '2020-05-04'
 	},
 
 	user0006: {
-		expires:1591421400000
+		expires: '2020-05-04'
 	},
 
 	user0007: {
-		expires: 1591483800000,
-		throttle: 6
+		expires: '2020-05-07'
 	},
 
 	user0008: {
-		expires: 1591483800000,
-		throttle: 6
+		expires: '2020-05-07'
 	},
 
 	user0009: {
-		expires: 1591565400000
+		expires: '2020-05-15 07:00'
 	},
 
 	user0010: {
-		expires: 1591486200000
+		expires: '2020-05-07'
 	},
 
+	user0011: {
+		expires: '2020-05-07'
+	},
+
+
 	user0012: {
-		expires: 1591498800000
+		expires: '2020-05-07'
 	},
 
 	user0013: {
-		expires: 1591560000000
+		expires: '2020-05-07 13:00'
 	},
 
 	user0014: {
-		expires: 1591567200000,
-		throttle: 7
+		expires: '2020-05-07 14:45'
 	},
 
+	user0015: {
+		expires: '2020-05-08 14:00'
+	},
+
+	user0016: {
+		expires: '2020-05-09 17:00'
+	},
+
+	user0017: {
+		expires: '2020-05-09 18:00'
+	},
+
+	user0018: {
+		expires: '2020-05-10 13:30'
+	},
+
+	user0019: {
+		expires: '2020-05-03 21:30'
+	},
+	user0020: {
+		expires: '2020-05-10 21:30'
+	},
+
+	user0021: {
+		expires: '2020-05-12 14:00'
+	},
+
+	user0022: {
+		expires: '2020-05-12 14:00'
+	},
+
+	user0023: {
+		expires: '2020-05-13 21:00'
+	},
+
+	user0024: {
+		expires: '2020-05-14 03:00'
+	},
+
+
 	vna002: {
-		
+
 	}
 }
